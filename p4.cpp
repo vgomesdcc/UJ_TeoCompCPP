97a281a05903dc87830de8a9e82af0453ece285f
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2021-09-11 12:43:05 -0300

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <math.h>
-#include<string>
+#include <iomanip>
 using namespace std;
-int main() {
-	int numerador,denominador;
-	cin>>numerador>>denominador;
-	if(numerador%denominador==0){
-		cout<<"Divisores perfeitos\n";
+int main () {
+	int perfeito1,perfeito2, flag1=0, flag2=0;
+	cin>>perfeito1>>perfeito2;
+	
+	for(int i=1; i<perfeito1; i++){
+		if(perfeito1%i==0){
+			flag1+=i;
+		}
 	}
 	
+	for(int z=1; z<perfeito2; z++){
+		if(perfeito2%z==0){
+			flag2+=z;
+		}
+	}
+	
+	if(perfeito1==flag1 && perfeito2==flag2){
+		cout<<"Sao divisores perfeitos";
+	}
 	else
 		cout<<"Nao sao divisores perfeitos\n";
 }
